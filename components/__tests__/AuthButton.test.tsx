22c814a68c2105c0c1f4312712a2646eafcac588
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:47:30 -0300

    Delete components directory

--- components/__tests__/AuthButton.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AuthButton from '../AuthButton';
-test('renders sign in button when unauthenticated', () => {
-  render(<AuthButton />);
-  expect(screen.getByText(/Sign In/i)).toBeInTheDocument();
-});
