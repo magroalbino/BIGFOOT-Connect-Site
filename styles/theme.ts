d1a16c44a8afa82f14c2b38aa10943483fe51feb
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:48:38 -0300

    Delete styles directory

--- styles/theme.ts
+++
@@ -1,8 +0,0 @@
-// styles/theme.ts
-export const theme = {
-  primary: '#DFFF9B', // verde neon claro
-  secondary: '#000000', // preto
-  background: '#F9FFF2',
-  text: '#1A1A1A',
-  success: '#B9FF66',
-};
