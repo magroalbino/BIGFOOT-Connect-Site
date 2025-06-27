3df3593f593054ed1646f7a0f0f3454f5f8cefcd
Fabrício Ricard <fabricioricard23@gmail.com>
2025-06-27 12:47:21 -0300

    Delete app directory

--- app/_not-found/layout.tsx
+++
@@ -1,8 +0,0 @@
-// app/_not-found/layout.tsx
-export default function NotFoundLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="pt-BR">
-            <body>{children}</body>
-        </html>
-    );
-}
--- app/test/page.tsx
+++
@@ -1,4 +0,0 @@
-// app/test/page.tsx
-export default function TestPage() {
-    return <div style={{ padding: '3rem', color: 'white' }}>Página de teste carregada com sucesso</div>;
-}
